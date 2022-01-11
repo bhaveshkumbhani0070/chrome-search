8890372193e40dd61653f021b8ee8875174d8e85
bhaveshkumbhani0070 <kumbhani.bhavesh@alps.cl>
2022-01-11 09:01:39 +0530

    Udatefor spacebar

--- public/loader.js
+++ public/loader.js
@@ -1,19 +1,24 @@
 let shown = false;
 
-const condition = (e) => {
-  if (e.clientY <= 0) {
-    console.log("e", e);
-  }
+const mouse_condition = (e) => {
   return e.clientY <= 0;
 };
+const condition = (e) => e.code === "Space" && e.target === document.body;
 
 // const on_mouse_move = (e) => {
 //   if (!condition(e)) return;
 //   step();
 // };
 const on_mouseout = (e) => {
+  if (!mouse_condition(e)) return;
+  step();
+};
+const on_key_down = (e) => {
   if (!condition(e)) return;
   step();
+  e.preventDefault();
+  e.stopImmediatePropagation();
+  return false;
 };
 
 const step = () => {
@@ -38,20 +43,30 @@ const show = () => {
   //   url: "index.html",
   // });
   console.log("show popup");
+  var element = document.createElement("div");
+  element.style.textAlign = "center";
+  element.style.width = "100%";
+  element.style.position = "fixed";
+  element.style.top = "0px";
+  element.style.left = "0px";
+  element.style.right = "0px";
+  element.style.bottom = "0px";
+  element.style.zIndex = "9000000000000000000";
+
   var iframe = document.createElement("iframe");
   iframe.style.background = "white";
   iframe.style.height = "400px";
   iframe.style.width = "800px";
-  iframe.style.position = "fixed";
-  iframe.style.top = "0px";
   iframe.style.right = "50%";
   iframe.style.zIndex = "9000000000000000000";
   iframe.style.border = "0px";
   iframe.src = chrome.runtime.getURL("index.html");
 
-  document.body.appendChild(iframe);
+  element.appendChild(iframe); // appends the iframe (element2) to div (element)
+  document.body.appendChild(element);
 };
 window.onload = function () {
   // this.addEventListener("mousemove", on_mouse_move);
   this.addEventListener("mouseout", on_mouseout);
+  this.addEventListener("keydown", on_key_down);
 };
